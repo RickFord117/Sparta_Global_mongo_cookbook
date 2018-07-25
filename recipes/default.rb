55f6560bf3179d28f15b95646bc5d3ebf0cc1ffd
RickFord117 <rhenriques@spartaglobal.com>
2018-07-25 09:24:46 +0100

    fixed file issue

---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: mongo
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+include_recipe "mongodb::default"
---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook:: mongo
+# Spec:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'mongo::default' do
+  context 'When all attributes are default, on Ubuntu 16.04' do
+    let(:chef_run) do
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+
+  it 'should install mongodb' do
+    expect(chef_run).to include_recipe('mongodb::install')
+  end
+
+end
